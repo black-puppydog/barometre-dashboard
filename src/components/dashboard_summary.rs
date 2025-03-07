20d62b78acf42eb0cd11827375077f53bd1b22d5
Daan Wynen <black.puppydog@gmx.de>
2025-03-07 10:59:50 +0100

    first dashboard works

--- src/components/dashboard_summary.rs
+++ src/components/dashboard_summary.rs
@@ -4,19 +4,29 @@ use crate::types::CommunePropertiesSlim;
 
 #[component]
 pub fn DashboardSummary(progresses: Vec<CommunePropertiesSlim>) -> Element {
+    let qualified = progresses.iter().filter(|c| c.progress() >= 100f32).count();
+    let close = progresses
+        .iter()
+        .filter(|c| c.target_contributions() - c.contributions as usize <= 10)
+        .count();
     rsx! {
         div{
-            id: "dashboard",
-            class: "w-full rounded-xl border-2 border-yellow-200 grid grid-cols-3",
+            class: "w-3/4 rounded-xl grid grid-cols-3 m-auto my-5",
             div{
-                class: "text-center text-green-400 bg-gray-300 border-blue rounded m-5",
-                "1. header"
+                class: "text-center border-2 border-gray-500 rounded-2xl m-5 p-5 text-xl font-semibold",
+                "{progresses.len()} communes"
             },
-            div { class: "text-center text-green-400 rounded, border-green-300 border-2",
-                "2. header"
+            div {
+                class: "text-center border-2 border-gray-500 rounded-2xl m-5 p-5 text-xl font-semibold",
+                "{qualified}",
+                br{},
+                "qualifiées",
             },
-            div { class: "text-center text-bold text-red-400 rounded border-red-400 border-2",
-                "3. header"
+            div {
+                class: "text-center border-2 border-gray-500 rounded-2xl m-5 p-5 text-xl font-semibold",
+                "{close}",
+                br{},
+                "presque qualifiées"
             }
         }
     }
